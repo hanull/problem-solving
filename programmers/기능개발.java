35feac94c3fcea88f8d3157513fe1188d574ca4c
Hanul Lee <46413629+hanull@users.noreply.github.com>
2019-11-16 21:08:31 +0900

    correct : 기능개발

---
+++ programmers/기능개발.java
@@ -0,0 +1,44 @@
+public static int[] solution(int[] progresses, int[] speeds) {
+    int[] arr=new int[progresses.length];
+    ArrayList<Integer> list = new ArrayList<>();
+    //Queue<Integer> list = new LinkedList<Integer>();
+    for(int i=0; i<progresses.length; i++) {
+      int cnt=(100-progresses[i])/speeds[i];
+      int tmp=(100-progresses[i])%speeds[i];
+      if(tmp>0) cnt++;
+      arr[i]=cnt;
+      list.add(cnt);
+    }
+    Queue<Integer> q = new LinkedList<Integer>();
+    boolean[] visited = new boolean[list.size()];
+    int first=arr[0];
+    int cnt=0;
+    int index=0;
+    while(true) {
+      if(index==list.size()) {
+        q.add(cnt);
+        break;
+      }
+      if(visited[index]) {
+        index++;
+        continue;
+      }
+      if(first>=list.get(index)) {
+        cnt++;
+        visited[index]=true;
+      }
+      else {
+        first=list.get(index);
+        q.add(cnt);
+        cnt=0;
+      }
+    }
+    int[] answer = new int[q.size()];
+    int len = q.size();
+    for(int i=0; i<len; i++) {
+      int num=q.poll();
+      answer[i]=num;
+      System.out.println(answer[i]);
+    }
+    return answer;
+  }
