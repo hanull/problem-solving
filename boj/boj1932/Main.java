a7b6001d3dca93bb4777e8074089aa421c09110e
hanull <dkfghg3@naver.com>
2020-09-21 15:26:20 +0900

    sovle : boj1932, 9252

--- boj/boj1932/Main.java
+++ boj/boj1932/Main.java
@@ -1,8 +1,48 @@
 package boj.boj1932;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = stoi(br.readLine());
+        int[][] dp = new int[n][n];
 
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = stoi(st.nextToken());
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    dp[i][j] += dp[i-1][j];
+                } else if (i == j) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] += Integer.max(dp[i - 1][j], dp[i - 1][j - 1]);
+                }
+            }
+        }
+        System.out.println(getMax(dp, n));
+    }
+
+    private static int getMax(int[][] dp, int n) {
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (res < dp[n - 1][i]) {
+                res = dp[n-1][i];
+            }
+        }
+        return res;
+    }
+
+    private static int stoi(String input) {
+        return Integer.valueOf(input);
     }
 }
--- boj/boj9252/Main.java
+++ boj/boj9252/Main.java
@@ -6,38 +6,40 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String input1 = br.readLine();
         String input2 = br.readLine();
         int len1 = input1.length();
         int len2 = input2.length();
-        int[][] dp = new int[len2 + 1][len1 + 1];
+        int[][] dp = new int[len1 + 1][len2 + 1];
 
-        for (int i = 1; i <= len2; i++) {
-            for (int j = 1; j <= len1; j++) {
-                if (input1.charAt(j - 1) == input2.charAt(i - 1)) {
+        for (int i = 1; i <= len1; i++) {
+            char c1 = input1.charAt(i - 1);
+            for (int j = 1; j <= len2; j++) {
+                char c2 = input2.charAt(j - 1);
+                if (c1 == c2) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                    dp[i][j] = Integer.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
         StringBuilder sb = new StringBuilder();
-        int r = len2;
-        int c = len1;
-        while (dp[r][c] != 0) {
-            if (dp[r - 1][c] == dp[r][c]) {
-                r--;
-            } else if (dp[r][c - 1] == dp[r][c]) {
-                c--;
-            } else if (dp[r][c] - 1 == dp[r - 1][c - 1]) {
-                sb.append(input1.charAt(c - 1));
-                r--;
-                c--;
+        int i = len1;
+        int j = len2;
+        int max = dp[i][j];
+        while (dp[i][j] != 0) {
+            if (dp[i - 1][j] == max) {
+                i--;
+            } else if (dp[i][j - 1] == max) {
+                j--;
+            } else {
+                sb.append(input1.charAt(i - 1));
+                max--;
+                i--;
+                j--;
             }
         }
-        bw.write(dp[len2][len1] + "\n" + sb.reverse().toString());
-        br.close();
-        bw.close();
+        System.out.println(dp[len1][len2]);
+        System.out.println(sb.reverse().toString());
     }
 }
