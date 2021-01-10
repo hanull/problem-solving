7fd1ce23c4d6d3896efecf3e25da257f49b28196
hanull <dkfghg3@naver.com>
2021-01-10 17:44:44 +0900

    알고리즘 dp 3문제 완료

--- boj/boj1149/Main3.java
+++ boj/boj1149/Main3.java
@@ -1,45 +1,43 @@
-package boj.boj1932;
+package boj.boj1149;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main3 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = stoi(br.readLine());
-        int[][] arr = new int[n][n];
-        int[][] dp = new int[n][n];
+        int[][] arr = new int[n][3];
+        int[][] dp = new int[n][3];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j <= i; j++) {
+            for (int j = 0; j < 3; j++) {
                 arr[i][j] = stoi(st.nextToken());
             }
         }
 
         dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == 0) {
-                    dp[i][0] = dp[i - 1][0] + arr[i][0];
-                } else if (j == i) {
-                    dp[i][i] = dp[i - 1][i - 1] + arr[i][i];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j - 1] + arr[i][j], dp[i - 1][j] + arr[i][j]);
-                }
-            }
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + arr[i][2];
         }
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, dp[n - 1][i]);
+
+        int min = 1000000;
+        for (int num : dp[n - 1]) {
+            min = Math.min(num, min);
         }
-        System.out.println(max);
+        System.out.println(min);
     }
 
     public static int stoi(String input) {
         return Integer.valueOf(input);
     }
+
 }
