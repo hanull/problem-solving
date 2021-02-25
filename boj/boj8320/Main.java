63bfadf8f4b934665b1e45afc7682c8b27ee5cc7
hanull <dkfghg3@naver.com>
2021-02-26 00:19:11 +0900

    solve: boj 직사각형 만들기

---
+++ boj/boj8320/Main.java
@@ -0,0 +1,18 @@
+package boj.boj8320;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            for (int j = i; i * j <= n; j++) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
