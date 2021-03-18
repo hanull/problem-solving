16f0abf7c7f96c70851530b365d09b2cebc43fea
hanull <dkfghg3@naver.com>
2021-03-18 20:49:25 +0900

    sovle: 정올 종교(유니온파인드)

---
+++ jungol/종교/Main.java
@@ -0,0 +1,57 @@
+package jungol.종교;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = stoi(st.nextToken());
+            int y = stoi(st.nextToken());
+            union(x, y);
+        }
+
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            if (i == parent[i]) cnt++;
+        }
+        System.out.println(cnt);
+    }
+
+    static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+
+    static int stoi(String input) {
+        return Integer.parseInt(input);
+    }
+
+}
