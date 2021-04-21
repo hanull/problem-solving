6e26842084568df3afa1caf9d07a9f7d89164c22
hanull <dkfghg3@naver.com>
2021-04-21 20:49:10 +0900

    solve: 백준-키순서

---
+++ SWEA/키순서/Solution.java
@@ -0,0 +1,62 @@
+package SWEA.키순서;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    static final int MAX = 987654321;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = stoi(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int N = stoi(br.readLine());
+            int M = stoi(br.readLine());
+            int[][] matrix = new int[N + 1][N + 1];
+            for (int i = 1; i <= N; i++) {
+                Arrays.fill(matrix[i], MAX);
+                matrix[i][i] = 0;
+            }
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int from = stoi(st.nextToken());
+                int to = stoi(st.nextToken());
+                matrix[from][to] = 1;
+            }
+
+            for (int k = 1; k <= N; k++) {
+                for (int i = 1; i <= N; i++) {
+                    for (int j = 1; j <= N; j++) {
+                        if (matrix[i][j] > matrix[i][k] + matrix[k][j]) {
+                            matrix[i][j] = matrix[i][k] + matrix[k][j];
+                        }
+                    }
+                }
+            }
+
+            int answer = 0;
+            for (int i = 1; i <= N; i++) {
+                boolean flag = true;
+                for (int j = 1; j <= N; j++) {
+                    if (i==j) continue;
+                    if (matrix[i][j] == MAX && matrix[j][i] == MAX) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) answer++;
+            }
+            System.out.println("#" + tc + " " + answer);
+
+        }
+    }
+
+    private static int stoi(String input) {
+        return Integer.parseInt(input);
+    }
+}
