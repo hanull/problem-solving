8ca64f6a50395b2e8dc7a3c1c4bf5055f7f9ebf6
hanull <dkfghg3@naver.com>
2021-06-11 14:02:52 +0900

    solve: 백준-n과m(2)

---
+++ boj/boj15650/Main.java
@@ -0,0 +1,31 @@
+package boj.boj15650;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int N, M;
+    static int[] pick;
+    static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        pick = new int[M];
+        dfs(1, 0);
+        System.out.print(answer);
+    }
+
+    private static void dfs(int index, int count) {
+        if (count == M) {
+            for (int i = 0; i < M; i++) answer.append(pick[i]).append(" ");
+            answer.append("\n");
+            return;
+        }
+        for (int i = index; i <= N; i++) {
+            pick[count] = i;
+            dfs(i + 1, count + 1);
+        }
+    }
+}
