6c89875d24e2376313e24604147e8458d27a2c2a
hanull <dkfghg3@naver.com>
2021-07-10 15:20:08 +0900

    버블 정렬 구현

--- Basic/sort/BubbleSort.java
+++ Basic/sort/BubbleSort.java
@@ -4,25 +4,23 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
-    static int[] arr = {3, 2, 4, 5, 1};
-    static int N;
-
-    public static void main(String[] args) {
-        N = arr.length;
-        bubbleSort();
-        System.out.println(Arrays.toString(arr));
-    }
-
-    static void bubbleSort() {
-        for (int i = N - 1; i >= 1; i--) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
+    static int[] bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j]  = temp;
                 }
             }
         }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 2, 4, 5, 1};
+        int[] answer = bubbleSort(array);
+        System.out.println(Arrays.toString(answer));
     }
 
 }
