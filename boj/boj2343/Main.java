c6649e0ac0db3829749810f812a31fae3a8b10e6
hanull <dkfghg3@naver.com>
2021-07-24 18:54:20 +0900

    refactor: 불필요한 배열 및 로직 삭제

--- boj/boj2343/Main.java
+++ boj/boj2343/Main.java
@@ -3,7 +3,6 @@ package boj.boj2343;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -17,28 +16,24 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int left = 0;
         int right = 0;
-        int minValue = 0;
         for (int i = 0; i < N; i++) {
             lessonList[i] = stoi(st.nextToken());
-            minValue = Math.max(minValue, lessonList[i]);
+            left = Math.max(left, lessonList[i]);
             right += lessonList[i];
         }
-        int[] bluRay = new int[N + 1];
         int answer = Integer.MAX_VALUE;
         while (left <= right) {
             int mid = (left + right) / 2;
             int total = 0;
             int count = 0;
-            Arrays.fill(bluRay, 0);
             for (int i = 0; i < N; i++) {
                 total += lessonList[i];
                 if (total > mid) {
-                    bluRay[count++] = total - lessonList[i];
                     total = lessonList[i];
+                    count++;
                 }
             }
-            bluRay[count] = total;
-            if (count + 1 > M || mid < minValue) {
+            if (count + 1 > M) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
