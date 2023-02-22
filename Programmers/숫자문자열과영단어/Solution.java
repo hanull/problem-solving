2e2ee809c8802989bab09291d2df4b1958059aaf
hanull <dkfghg3@naver.com>
2023-02-22 18:58:55 +0900

    solve: 프로그래머스/숫자 문자열과 영단어

---
+++ Programmers/숫자문자열과영단어/Solution.java
@@ -0,0 +1,15 @@
+package Programmers.숫자문자열과영단어;
+
+public class Solution {
+
+    public int solution(String s) {
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        for (int i = 0; i < 10; i++) {
+            s = s.replaceAll(alphabets[i], digits[i]);
+        }
+
+        return Integer.parseInt(s);
+    }
+}
